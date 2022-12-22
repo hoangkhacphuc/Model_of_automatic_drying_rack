3c4dfbbe8e0f9720752e839fa3301747d8cd38c6
hoangkhacphuc <hoangkhacphuc.dev@gmail.com>
2022-12-22 17:56:18 +0700

    test multi thread

--- Example/Project/main.cpp
+++ Example/Project/main.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <wiringPi.h>
 #include <pthread.h>
 
 #define LED 0
 #define LED2 2
 #define TIME_LED 500
-
-// void *led()
-// {
-//     while (true){
-// 		digitalWrite(LED,HIGH);
-//         delay(TIME_LED);
-// 		digitalWrite(LED,LOW);
-//         delay(TIME_LED);
-// 	}
-// }
-
-// void *led2()
-// {
-//     while (true){
-// 		digitalWrite(LED2,HIGH);
-//         delay(TIME_LED);
-// 		digitalWrite(LED2,LOW);
-//         delay(TIME_LED);
-// 	}
-// }
 
 void *led(void *arg)
 {
@@ -47,8 +28,6 @@ void *led2(void *arg)
     }
 }
 
-
-
 int main(void){
 	wiringPiSetup();
 	pinMode(LED, OUTPUT);
@@ -56,10 +35,9 @@ int main(void){
 
     pthread_t thread1, thread2;
     pthread_create(&thread1, NULL, led, NULL);
-    delay(300);
     pthread_create(&thread2, NULL, led2, NULL);
     pthread_join(thread1, NULL);
-    delay(500);
+    delay(300);
     pthread_join(thread2, NULL);
 	return 0;
 }
