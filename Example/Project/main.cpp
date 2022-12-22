d4096e591ba5fd7190ab39a85fccaab9ba3332c3
hoangkhacphuc <hoangkhacphuc.dev@gmail.com>
2022-12-22 17:14:34 +0700

    add folder project

--- Example/Project/main.cpp
+++ Example/Project/main.cpp
@@ -5,11 +5,17 @@
 int main(void){
 	wiringPiSetup();
 	pinMode(LED, OUTPUT);
-	for(;;){
+	led();
+	return 0;
+}
+
+void led(int time)
+{
+    if (time == 0) time = 500;
+    while (true){
 		digitalWrite(LED,HIGH);
         delay(500);
 		digitalWrite(LED,LOW);
         delay(500);
 	}
-	return 0;
 }
