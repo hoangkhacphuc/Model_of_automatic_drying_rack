b0362d33897a522c4f61cfb307299274f67fb655
hoangkhacphuc <hoangkhacphuc.dev@gmail.com>
2022-12-25 23:50:47 +0700

    update code project python

--- Example/Project/main.py
+++ Example/Project/main.py
@@ -30,7 +30,8 @@ class Button:
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def is_pressed(self):
-        print('Pressed\n')
+        if GPIO.input(self.pin) == GPIO.LOW:
+            print('Pressed\n')
         return GPIO.input(self.pin) == GPIO.LOW
 
     def cleanup(self):
